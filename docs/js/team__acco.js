1a30b87a0c09f6179acf1bc0f7972c3310e688ae
Sergei Brodnikov <45542499+panbro@users.noreply.github.com>
2019-02-11 20:24:55 +0300

    week2(second)

---
+++ docs/js/team__acco.js
@@ -0,0 +1,39 @@
+// const items = document.querySelectorAll('.crew__item');
+
+// for (item of items) {
+
+
+//   item.addEventListener('click', e => {
+//     const curItem = e.currentTarget;
+//     const content = curItem.querySelector('.crew__thumb');
+//     console.log(content);
+
+//     const browserBlock = content.children[1].children[1];
+//     console.log(browserBlock + ' browserBlock');
+//   });
+// }
+
+
+    
+
+    // const nameBlock = browserBlock.children[1];
+    // console.log(nameBlock + ' nameBlock');
+
+    // const textBlock = browserBlock.children[2];
+    // console.log(textBlock + ' textBlock');
+
+    //const nameBlock = document.querySelector('.crew__post');
+    //const textBlock = content.childNodes[1];
+    //console.log(textBlock);
+    //const reqHeightBrowser = textBlock.getBoundingClientRect().height;
+    // const reqHeightBrowser = textBlock.getBoundingClientRect().height + nameBlock.getBoundingClientRect().height; 
+    // content.style.height = '100%';
+    // curItem.className += ' crew__item_active';
+    //console.log(reqHeightBrowser);
+
+
+
+// const isClicked = items.addEventListener('click') e => {
+//   console.log('yeah');
+// }
+
