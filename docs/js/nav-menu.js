8f348837dc9de1c798d7a6db1e04c8adc471f7b1
Sergei Brodnikov <45542499+panbro@users.noreply.github.com>
2019-02-18 01:29:24 +0300

    week3_1

---
+++ docs/js/nav-menu.js
@@ -0,0 +1,31 @@
+const hambMenu = document.querySelector('.hamburger-menu-link');
+const navMenu = document.querySelector('.nav-menu');
+const logoFullMenu = document.querySelector('.logo__full-menu');
+const closeMenu = document.querySelector('.close__menu');
+
+
+const hambClick = hambMenu.addEventListener('click', e=> {
+  navMenu.style.display = 'block';
+  e.preventDefault();
+  document.body.style.overflow = 'hidden';
+});
+
+const closeFullMenuByCloseIcon = closeMenu.addEventListener('click', e=>{
+  navMenu.style.display = 'none';
+  document.body.style.overflow = '';
+})
+const closeFullMenuByLogoIcon = logoFullMenu.addEventListener('click', e=>{
+  navMenu.style.display = 'none';
+  document.body.style.overflow = '';
+})
+
+var menuItem = document.querySelectorAll('.nav-menu__item');
+for (const item of menuItem) {
+  item.addEventListener('click', e=> {
+    navMenu.style.display = 'none';
+    document.body.style.overflow = '';
+  });
+}
+
+
+
