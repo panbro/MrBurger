1e2854e7b3c3d242bac296e9c7307c4ce70205d1
Sergei Brodnikov <45542499+panbro@users.noreply.github.com>
2019-02-27 03:49:40 +0300

    week5_2

--- docs/js/yandexMap.js
+++ docs/js/yandexMap.js
@@ -7,22 +7,22 @@ function init() {
 
   let myPlacemark1 = new ymaps.Placemark([59.896, 30.295], {}, {
         iconLayout: 'default#image',
-        iconImageHref: './../img/icons/map-marker.svg',
+        iconImageHref: './img/icons/map-marker.svg',
         iconImageSize: [95, 60]
       });
   let myPlacemark2 = new ymaps.Placemark([59.962, 30.293], {}, {
         iconLayout: 'default#image',
-        iconImageHref: './../img/icons/map-marker.svg',
+        iconImageHref: './img/icons/map-marker.svg',
         iconImageSize: [95, 60]
       });
   let myPlacemark3 = new ymaps.Placemark([59.952481, 30.36526], {}, {
         iconLayout: 'default#image',
-        iconImageHref: './../img/icons/map-marker.svg',
+        iconImageHref: './img/icons/map-marker.svg',
         iconImageSize: [95, 60]
       });
   let myPlacemark4 = new ymaps.Placemark([59.893539, 30.46659], {}, {
         iconLayout: 'default#image',
-        iconImageHref: './../img/icons/map-marker.svg',
+        iconImageHref: './img/icons/map-marker.svg',
         iconImageSize: [95, 60]
       });
   myMap.geoObjects.add(myPlacemark1);
